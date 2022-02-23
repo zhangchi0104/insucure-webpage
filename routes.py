d6ac3ca76918c55cb0e90e0afa693f608f19b237
Alex Zhang <zhangchi0104@live.com>
2022-02-23 16:55:37 +0800

    finished homepage

--- routes.py
+++ routes.py
@@ -3,6 +3,25 @@ from flask import Blueprint, render_template
 routes = Blueprint('/', __name__, template_folder='templates')
 
 
+def _mk_attack_struct(name: str, desc):
+    route = name.lower().strip().replace(' ', '-')
+    return {
+        'name': name,
+        'route': route,
+        'description': desc,
+        'template': f'{route}.html'
+    }
+
+
+ATTACKS = [
+    _mk_attack_struct(
+        'Crendential Stuffing',
+        'Attackers collect leaked credentials and use them to access '
+        'the user\'s other accounts in case they use the same passwords',
+    )
+]
+
+
 @routes.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', attack_types=ATTACKS)
