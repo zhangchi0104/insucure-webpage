8d1a221c0f9f90efb62b23bca98ca0b0ec57474d
Alex Zhang <zhangchi0104@live.com>
2022-02-24 22:07:00 +0800

    done credential stuffing

--- routes.py
+++ routes.py
@@ -12,7 +12,7 @@ def _get_source_code(path: str, base_dir='demo'):
     return ''.join(lines)
 
 
-def _mk_attack_struct(name: str, desc):
+def _mk_route_struct(name: str, desc):
     route = name.lower().strip().replace(' ', '-')
     template_name = route.replace('-', '_')
     return {
@@ -20,30 +20,27 @@ def _mk_attack_struct(name: str, desc):
         'route': route,
         'description': desc,
         'template': f'{template_name}.html',
-        'source': {
-            "test.py": _get_source_code('test.py')
-        }
     }
 
 
-ATTACKS = [
-    _mk_attack_struct(
+ROUTE_CONFIG = [
+    _mk_route_struct(
         'Credential Stuffing',
         'Attackers collect leaked credentials and use them to access '
         'the user\'s other accounts in case they use the same passwords',
     )
 ]
 
-for attack in ATTACKS:
+for attack in ROUTE_CONFIG:
     routes.add_url_rule(
         f'/{attack["route"]}',
         view_func=lambda: render_template(
             attack['template'],
-            source=attack['source'],
+            source=attack.get('source', None),
         ),
     )
 
 
 @routes.route('/')
 def index():
-    return render_template('index.html', attack_types=ATTACKS)
+    return render_template('index.html', attack_types=ROUTE_CONFIG)
