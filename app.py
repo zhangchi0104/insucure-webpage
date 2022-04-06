1282fc38095de93a5613e5781f3318b94ca9cece
Alex Zhang <zhangchi0104@live.com>
2022-04-06 11:22:29 +0800

    renamed

--- app.py
+++ app.py
@@ -1,46 +1,23 @@
-# Author: Chi Zhang z5211214
-# Date: 22/02/2022
-
 from flask import Flask, render_template
-from utils import mk_route_struct
-from routes import  register_routes
+from os import getenv
+from datetime import datetime
 
-app = Flask(__name__)
-register_routes(app)
-
-
-ROUTE_CONFIG = {
-    **mk_route_struct(
-        'Credential Stuffing',
-        'Attackers collect leaked credentials and use them to access '
-        'the user\'s other accounts in case they use the same passwords',
-    ),
-    **mk_route_struct(
-        'Template Injection',
-        'If the the website uses template to render content, '
-        'attackers can inject code wchich will be executed on the server.',
-    ),
-    **mk_route_struct(
-        'XSS Attack',
-        'Attackers can send a web app malicious code, which allows '
-        'the attacker gain access to sensitive information',
-    ),
-    **mk_route_struct(
-        'CSRF Attack',
-        'Attackers force anthenticated users to send unwanted request'
-        'to web applications',
-    ),
-    **mk_route_struct(
-        'SQL Injection',
-        "Attackers access the database by injecting SQL statements into"
-        "a vulnerable input field",
-    )
-}
+app = Flask(__name__, template_folder='.')
+CSRF_ENDPOINT = getenv('CSRF_ENDPOINT', "http://127.0.0.1/csrf-attack")
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', attack_types=ROUTE_CONFIG.values())
-if __name__ == '__main__':
-    print(app.url_map)
-    app.run("0.0.0.0", debug=True)
+    return render_template(
+        './index.html',
+        csrf_endpoint=CSRF_ENDPOINT,
+        timestamp=datetime.now(),
+    )
+
+
+if __name__ == "__main__":
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
